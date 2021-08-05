a36259f652efa9da5109ec4ba5534340f51f892c
Idin Khayami <idin.khanoom.khayami@gmail.com>
2021-08-05 18:54:47 +0430

    fix: resolve conflicts and remove yarn.lock

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,4 @@
 module.exports = {
   "**/*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
-  "**/*.{md,json}": ["prettier --write"],
-  "**/*.css": ["stylelint --fix", "prettier --write"],
-  "**/*.scss": ["stylelint --syntax scss --fix", "prettier --write"],
+  "**/*.{md,json}": ["prettier --write"]
 };
