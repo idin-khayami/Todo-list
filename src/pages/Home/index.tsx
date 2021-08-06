42b69cace6bc8b7663473ad97a6b06e5f1e19b47
Idin Khayami <idin.khanoom.khayami@gmail.com>
2021-08-06 20:28:01 +0430

    fix: clean home style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,34 +1,19 @@
-import Paper from '@material-ui/core/Paper'
-import { makeStyles } from '@material-ui/core/styles'
+import { Box } from '@material-ui/core'
 import AddNewTask from '../../components/AddNewTask'
 import NoResult from '../../components/NoResult'
 import PageHeader from '../../components/PageHeader'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    overflow: 'hidden',
-  },
-  paper: {
-    maxWidth: 500,
-    margin: `${theme.spacing(0)}px auto`,
-  },
-}))
-
 const Home = () => {
-  const classes = useStyles()
   return (
-    <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <PageHeader title="Home" />
-      </Paper>
-      <Paper className={classes.paper}>
+    <Box display="flex" flexDirection="column" flexGrow={1}>
+      <PageHeader title="Home" />
+      <Box width="100%">
         <AddNewTask />
-      </Paper>
-      <Paper className={classes.paper}>
+      </Box>
+      <Box width="100%">
         <NoResult />
-      </Paper>
-    </div>
+      </Box>
+    </Box>
   )
 }
 
