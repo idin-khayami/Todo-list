417a13ba42c38e02605a8ffdf271b3dea405c2f6
Idin Khayami <idin.khanoom.khayami@gmail.com>
2021-08-06 17:01:41 +0430

    feat: add text field component

---
+++ src/components/text-field/text-field.tsx
@@ -0,0 +1,34 @@
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+interface TextFieldsProps {
+  id: string;
+  label: string;
+  multiline?: boolean;
+}
+
+function TextFields({ id, label, multiline = false }: TextFieldsProps) {
+  const classes = useStyles();
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField
+        rows={4}
+        id={id}
+        label={label}
+        multiline={multiline}
+        variant="filled"
+        fullWidth
+      />
+    </form>
+  );
+}
+
+export default TextFields;
