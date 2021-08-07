48697cea2816f1f4f2175354ac0f9c4d6f68c7d9
Idin Khayami <idin.khanoom.khayami@gmail.com>
2021-08-07 22:12:32 +0430

    feat: add type files

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+type UUID = string
+
+interface BaseTask {
+  title: string
+  description: string
+}
+
+export interface Task extends BaseTask {
+  id: UUID
+  status: string
+}
+
+export type CreateTaskInput = BaseTask
+
+export type UpdateTaskInput = Task
