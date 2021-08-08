a5b432e6a2758518e5cf10efe6825e25685084b5
Idin Khayami <idin.khanoom.khayami@gmail.com>
2021-08-08 20:49:05 +0430

    fix: move status list object into constants

--- src/constants/status.ts
+++ src/constants/status.ts
@@ -1,10 +1,21 @@
+import { TaskStatus } from '../types'
+
 /**
  * Status
  */
 
-export const STATUS_STATE_TO_DO = 'ToDo'
-export const STATUS_STATE_IN_PROGRESS = 'InProgress'
-export const STATUS_STATE_BLOCKED = 'Blocked'
-export const STATUS_STATE_IN_QA = 'InQA'
-export const STATUS_STATE_DONE = 'Done'
-export const STATUS_STATE_DEPLOYED = 'Deployed'
+export const STATUS_TO_DO = 'ToDo'
+export const STATUS_IN_PROGRESS = 'InProgress'
+export const STATUS_BLOCKED = 'Blocked'
+export const STATUS_IN_QA = 'InQA'
+export const STATUS_DONE = 'Done'
+export const STATUS_DEPLOYED = 'Deployed'
+
+export const STATUS_LABEL_MAP: { [key in TaskStatus]: string } = {
+  [STATUS_TO_DO]: 'To Do',
+  [STATUS_IN_PROGRESS]: 'In Progress',
+  [STATUS_BLOCKED]: 'Blocked',
+  [STATUS_IN_QA]: 'In Qa',
+  [STATUS_DONE]: 'Done',
+  [STATUS_DEPLOYED]: 'Deployed',
+}
