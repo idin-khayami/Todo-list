9c0c5abf10990a08a639bbf7b21f8c0cd5d26177
Idin Khayami <idin.khanoom.khayami@gmail.com>
2021-08-08 16:28:44 +0430

    fix: change not found page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,26 @@
+import { Box, Link, Typography } from '@material-ui/core'
+import paths from '../../router/paths'
+
 const NotFound = () => {
-  return <p>Sorry, the page you are looking for does not exist.</p>
+  return (
+    <Box
+      height="100vh"
+      mx={2}
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      flexGrow={1}
+    >
+      <Box my={2} textAlign="center">
+        <Typography variant="h6" component="p" color="primary">
+          Sorry, the page you are looking for does not exist.
+        </Typography>
+        <Box my={3} textAlign="center">
+          <Link href={paths.taskList}>Go Home</Link>
+        </Box>
+      </Box>
+    </Box>
+  )
 }
 
 export default NotFound
