e2961bdf91cd2cc19c710c003cfe0a0acb18ab9d
Idin Khayami <idin.khanoom.khayami@gmail.com>
2021-08-08 20:48:31 +0430

    fix: refctor get status functions

---
+++ src/service/status/get-status-label.ts
@@ -0,0 +1,6 @@
+import { STATUS_LABEL_MAP } from '../../constants/status'
+import { TaskStatus } from '../../types'
+
+export const getStatusLabel = (status: TaskStatus): string => {
+  return STATUS_LABEL_MAP[status]
+}
