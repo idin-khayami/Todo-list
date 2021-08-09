4fed61760acd82c9bdd4222d5908ceb7a45baaf5
Idin Khayami <idin.khanoom.khayami@gmail.com>
2021-08-09 12:29:57 +0430

    fix: change task card max width

--- src/components/TaskCard/index.tsx
+++ src/components/TaskCard/index.tsx
@@ -13,7 +13,6 @@ const useStyles = makeStyles(
   () => ({
     textTruncate: {
       display: '-webkit-box',
-      maxWidth: '200px',
       WebkitBoxOrient: 'vertical',
       overflow: 'hidden',
     },
