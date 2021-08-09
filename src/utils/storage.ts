5a102bbce101ae816a345d6102be187bbe15cf24
Idin Khayami <idin.khanoom.khayami@gmail.com>
2021-08-09 20:16:41 +0430

    fix: remove storage utils

--- src/utils/storage.ts
+++
@@ -1,6 +0,0 @@
-export const getItem = (key: string): string | null => {
-  return localStorage.getItem(key)
-}
-
-export const setItem = (key: string, value: string) =>
-  localStorage.setItem(key, value)
