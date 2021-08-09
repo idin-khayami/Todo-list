61d02372ac7c33afbed42cd07b4367299d69f4b2
Idin Khayami <idin.khanoom.khayami@gmail.com>
2021-08-09 17:12:37 +0430

    fix: remove storage utils

--- src/service/storage/index.ts
+++ src/service/storage/index.ts
@@ -1,9 +1,8 @@
 import { Task } from 'types'
-import { getItem, setItem } from 'utils/storage'
 import { storageKeys } from './storage-keys'
 
 export const getTasksList = (): Task[] =>
-  JSON.parse(getItem(storageKeys.tasksList) ?? '[]')
+  JSON.parse(localStorage.getItem(storageKeys.tasksList) ?? '[]')
 
 export const setTasksList = (data: Task[]): void =>
-  setItem(storageKeys.tasksList, JSON.stringify(data))
+  localStorage.setItem(storageKeys.tasksList, JSON.stringify(data))
